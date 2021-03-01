c224a39a0e2286b3cb663198a91acf2f85c8a136
Danny van Os <dannyvanos93@gmail.com>
2021-03-01 21:46:15 +0100

    Left at page 197

--- SportsStore.Tests/CartPageTests.cs
+++ SportsStore.Tests/CartPageTests.cs
@@ -17,43 +17,82 @@ namespace SportsStore.Tests
         [Fact]
         public void Can_Load_Cart()
         {
-            // Arrange - create a mock repository
+            // Arrange - create a mock repo
             Product p1 = new Product { ProductID = 1, Name = "P1" };
             Product p2 = new Product { ProductID = 2, Name = "P2" };
-
             Mock<IStoreRepository> mockRepo = new Mock<IStoreRepository>();
-            mockRepo.Setup(m => m.Products).Returns((new Product[] {
+            mockRepo.Setup(m => m.Products).Returns((new Product[]
+            {
                 p1, p2
             }).AsQueryable<Product>());
-            
+
             // Arrange - create a cart
             Cart testCart = new Cart();
             testCart.AddItem(p1, 2);
             testCart.AddItem(p2, 1);
-            
-            // Arrange - create a mock page context and session
-            Mock<ISession> mockSession = new Mock<ISession>();
-            byte[] data =
-            Encoding.UTF8.GetBytes(JsonSerializer.Serialize(testCart));
-            mockSession.Setup(c => c.TryGetValue(It.IsAny<string>(), out data));
-            Mock<HttpContext> mockContext = new Mock<HttpContext>();
-            mockContext.SetupGet(c => c.Session).Returns(mockSession.Object);
-            
+
             // Act
-            CartModel cartModel = new CartModel(mockRepo.Object)
-            {
-                PageContext = new PageContext(new ActionContext
-                {
-                    HttpContext = mockContext.Object,
-                    RouteData = new RouteData(),
-                    ActionDescriptor = new PageActionDescriptor()
-                })
-            };
+            CartModel cartModel = new CartModel(mockRepo.Object, testCart);
             cartModel.OnGet("myUrl");
-            
+
             // Assert
             Assert.Equal(2, cartModel.Cart.Lines.Count());
             Assert.Equal("myUrl", cartModel.ReturnUrl);
         }
+
+        [Fact]
+        public void Can_Update_Cart()
+        {
+            // Arrange
+            // - create a mock repository
+            Mock<IStoreRepository> mockRepo = new Mock<IStoreRepository>();
+            mockRepo.Setup(m => m.Products).Returns((new Product[] {
+                new Product { ProductID = 1, Name = "P1" }
+            }).AsQueryable<Product>());
+
+            Cart testCart = new Cart();
+            
+            // Act
+            CartModel cartModel = new CartModel(mockRepo.Object, testCart);
+            cartModel.OnPost(1, "myUrl");
+            
+            // Assert
+            Assert.Single(testCart.Lines);
+            Assert.Equal("P1", testCart.Lines.First().Product.Name);
+            Assert.Equal(1, testCart.Lines.First().Quantity);
+        }
+
+        [Fact]
+        public void Can_Remove_Cart_Line()
+        {
+            // Arrange - create a mock repository
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+            Product p3 = new Product { ProductID = 4, Name = "P3" };
+            Product p4 = new Product { ProductID = 5, Name = "P4" };
+
+            Mock<IStoreRepository> mockRepo = new Mock<IStoreRepository>();
+            mockRepo.Setup(m => m.Products).Returns((new Product[]
+            {
+                p1,p2,p3,p4
+            }).AsQueryable<Product>());
+
+            // Arrange - setup the cart
+            Cart testCart = new Cart();
+            testCart.AddItem(p1, 1);
+            testCart.AddItem(p2, 2);
+            testCart.AddItem(p3, 3);
+            testCart.AddItem(p4, 4);
+
+            // Act
+            CartModel cartModel = new CartModel(mockRepo.Object, testCart);
+            cartModel.OnPostRemove(1, "myUrl");
+
+            // Assert
+            Assert.NotNull(testCart);
+            Assert.Equal("P2", testCart.Lines.First().Product.Name);
+            Assert.Equal(2, testCart.Lines.First().Quantity);
+        }
+
     }
 }
--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -29,6 +30,8 @@ namespace SportsStore
             services.AddRazorPages();
             services.AddDistributedMemoryCache();
             services.AddSession();
+            services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
