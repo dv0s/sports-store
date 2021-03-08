191d2a77758d7578c2e7f3b4ba1fdadc15c88571
Danny van Os <dannyvanos93@gmail.com>
2021-03-08 13:40:26 +0100

    Finished chapter 10 - Administering orders

--- SportsStore/Models/Order.cs
+++ SportsStore/Models/Order.cs
@@ -34,5 +34,7 @@ namespace SportsStore.Models
         public string Country { get; set; }
 
         public bool GiftWrap { get; set; }
+        [BindNever]
+        public bool Shipped { get; set; }
     }
 }
